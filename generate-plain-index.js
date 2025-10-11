4a7e13c2a3207882e1cf8f35ffc3a1e94da873a9
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-10-12 02:26:29 +0600

    Update generate-plain-index.js

--- generate-plain-index.js
+++ generate-plain-index.js
@@ -2,7 +2,11 @@
 const fs = require('fs');
 
 const apiURLs = [
-  "https://bonikbarta.com/api/post-filters/73?root_path=00000000010000000001"
+  "https://bonikbarta.com/api/post-filters/73?root_path=00000000010000000001",
+  "https://bonikbarta.com/api/post-lists/35?root_path=00000000010000000001",
+  "https://bonikbarta.com/api/post-lists/36?root_path=00000000010000000001",
+  "https://bonikbarta.com/api/post-lists/33?root_path=00000000010000000001",
+  "https://bonikbarta.com/api/post-lists/34?root_path=00000000010000000001"
 ];
 const baseURL = "https://bonikbarta.com";
 
